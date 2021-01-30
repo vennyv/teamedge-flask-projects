7da8fb315db5c57469b76fff90638d32a8de5536
Venessa <nneomav04@gmail.com>
2021-01-30 10:55:13 -0500

    Challenge 1

---
+++ rainbow/app.py
@@ -0,0 +1,35 @@
+@app.route('/')
+def index():
+    return "Welcome to Venessa\'s Rainbow Project"
+    return 'Welcome to Venessa\'s Rainbow Project'
+
+@app.route('/red')
+def red():
+    return render_template('red.html')
+
+@app.route('/orange')
+def orange():
+    return render_template('orange.html')
+
+@app.route('/yellow')
+def yellow():
+    return render_template('yellow.html')
+
+@app.route('/green')
+def green():
+    return render_template('green.html')
+
+@app.route('/blue')
+def blue():
+    return render_template('blue.html')
+
+@app.route('/indigo')
+def indigo():
+    return render_template('indigo.html')
+
+@app.route('/violet')
+def violet():
+    return render_template('violet.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
