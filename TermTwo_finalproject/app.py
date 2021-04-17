3fedf484867a8d8fe7b734e5ad26989fdae4fadd
venny <nneomav04@gmail.com>
2021-04-17 12:22:08 -0400

    Implemented sqlite database

--- TermTwo_finalproject/app.py
+++ TermTwo_finalproject/app.py
@@ -1,19 +1,44 @@
-from flask import Flask, render_template, request, current_app as app
-from sense_hat import SenseHat
+from flask import Flask, request, render_template, current_app as app  
+from sense_hat import SenseHat  
 from time import sleep
+import sqlite3
 
 app = Flask(__name__)
+sense = SenseHat()
+messages = {}
+sense.low_light = True
 
 @app.route('/')
 def index():
-    return render_template("index.html")
-    
-@app.route('/petpeeve', methods=['GET', 'POST']) 
-def petpeeve():
+    return render_template('index.html')
+
+@app.route('/sent', methods=['GET', 'POST'])
+def success():
     if request.method == "POST":
         message = request.form.get("message")
+        author = request.form.get("author")
+        messages[author] = message
         sense.show_message(message)
-    return render_template('index.html', message = message)
+
+        conn = sqlite3.connect("./static/data/messages.db")
+        curs = conn.cursor()
+        curs.execute("INSERT INTO messages VALUES((?),(?))", (author, message))
+        conn.commit()
+        conn.close()
+        sense.show_message(message)                                             
+    return render_template('sent.html', message = message, author = author)
+
+@app.route('/messages')
+def all_messages():
+    conn = sqlite3.connect("./static/data/messages.db")
+    curs = conn.cursor()
+    messages = []
+    rows = curs.execute("SELECT * FROM messages")
+    for row in rows:
+        message = {'name':row[0], 'message':row[1]}
+        messages.append(message)
+    conn.close()
+    return render_template('log.html', messages = messages)
 
 if __name__ == '__main__':
-   app.run(debug = True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
